d7b029640517b93aad3c690985a8fef670702d19
KomoriWu <564139666@qq.com>
2017-05-31 12:04:31 -0700

    5-31 关闭instant run 。（会与sugar冲突）

--- app/src/main/java/com/example/txtledbluetooth/main/MainActivity.java
+++ app/src/main/java/com/example/txtledbluetooth/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.txtledbluetooth.main;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
