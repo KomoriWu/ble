2664511c07afbef20838220f482bdff8f1d8f812
KomoriWu <tmwuzhen@qq.com>
2017-06-26 16:01:57 +0800

    06-26 加入广播优化notify

--- app/src/main/java/com/example/txtledbluetooth/main/model/MainModelImpl.java
+++ app/src/main/java/com/example/txtledbluetooth/main/model/MainModelImpl.java
@@ -87,7 +87,6 @@ public class MainModelImpl implements MainModel {
 
             @Override
             public void onSearchStopped() {
-                onInitBleListener.OnException(context.getString(R.string.search_stop));
             }
 
             @Override
